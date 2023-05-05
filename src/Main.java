0b2824bc86572d9ff65e68259615837f5e17ab1e
nikbatura <nikbatura@mail.ru>
2023-05-05 21:22:10 +0700

    Проверьте дз

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
         task2();
         task3();
         task4();
     }
 
-    public static void task1() {
-        System.out.println("задача 1");
-        String phone = "+960-415-75-37";
-        phone = phone.replace("-", "");
-        phone = phone.replace(" ", "");
-        phone = phone.replace("+", "");
-        if (phone.length() == 10) {
-            phone = '7' + phone;
-        } else if (phone.length() > 11) {
-            throw new RuntimeException("Телефон слишком длинный");
-        } else if (phone.length() < 10) {
-            throw new RuntimeException("Телефон слишком короткий");
-        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
-            throw new RuntimeException("Среди нас посторонний");
-        }
-        System.out.println("phone = " + phone);
-        String expectedPhone = "79604157537";
-        if (phone.equals(expectedPhone)) {
-            System.out.println("успех");
-        } else {
-            System.out.println("неудача");
-        }
-    }
     public static void task2() {
         System.out.println("Задача1");
         String firstName = "Nikita";
@@ -36,7 +12,7 @@ public class Main {
         String lastName = "Batura";
         String fullName = "Batura Nikita Alexandrovich";
         if (fullName.length() > 20) {
-            fullName = lastName + firstName + middleName;
+            fullName = lastName +" " + firstName +" " + middleName;
             System.out.println("ФИО сотрудника " + fullName);
         } else {
             System.out.println("Неверное имя");
